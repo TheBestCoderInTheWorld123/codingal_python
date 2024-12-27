9fd129e8e988e61b55f8ec8967e3e5b38915d44e
Siebel Suhail <72099341+TheBestCoderInTheWorld123@users.noreply.github.com>
2024-12-27 13:38:09 +0500

    Update Polygon Area Calculator(Inheritance & Abstraction).py

--- ACPS/Polygon Area Calculator(Inheritance & Abstraction).py
+++ ACPS/Polygon Area Calculator(Inheritance & Abstraction).py
@@ -1,51 +1,50 @@
-class Shapes:
+class Polygons:
     def area(self):
         pass
 
-class Square(Shapes):
-    def area(self):
-        sqr = int(input("What is the length of the square?\n"))
-        sqr_area = sqr*sqr
-        print("Area is", sqr_area)
-
-class Rectangle(Shapes):
-    def area(self):
-        rec_l = int(input("What is the length of the rectangle?\n"))
-        rec_w = int(input("What is the width of the rectangle?\n"))
-        rec_area = rec_l*rec_w
-        print("Area is", rec_area)
-        
-class Circle(Shapes):
-    def area(self):
-        circle_r = int(input("What is the radius of the circle?\n"))
-        circle_area = 3.14*circle_r*circle_r
-        print("Area is", circle_area)
-
-class Triangle(Shapes):    
+class Triangle(Polygons):    
     def area(self):
         tri_h = int(input("What is the height of the triangle?\n"))
         tri_b = int(input("What is the base of the triangle?\n"))
         tri_area = (tri_h*tri_b)/2
         print("Area is", tri_area)
 
-sqr = Square()
-rec = Rectangle()
-circle = Circle()
-tri = Triangle()
+class Square(Polygons):
+    def area(self):
+        sqr = int(input("What is the length of the square?\n"))
+        sqr_area = sqr*sqr
+        print("Area is", sqr_area)
 
-print("What shape's area do you want?\n1. Square\n2. Rectangle\n3. Circle\n4. Triangle")
+class Pentagon(Polygons):
+    def area(self):
+        pent_l = int(input("What is the length of the pentagon?\n"))
+        pent_area = 0.25*3.077*pent_l*pent_l
+        print("Area is", pent_area)
+        
+class Hexagon(Polygons):
+    def area(self):
+        hex_l = int(input("What is the length of the hexagon?\n"))
+        hex_area = 0.5*1.732*3*hex_l*hex_l
+        print("Area is", hex_area)
+
+tri = Triangle()
+sqr = Square()
+pent = Pentagon()
+hex = Hexagon()
+
+print("What shape's area do you want?\n1. Triangle\n2. Square\n3. Pentagon\n4. Hexagon")
 ch = int(input())
 if(ch == 1):
-    print("\nSquare")
-    sqr.area()
-elif(ch == 2):
-    print("\nRectangle")
-    rec.area()
-elif(ch == 3):
-    print("\nCircle")
-    circle.area()
-elif(ch == 4):
     print("\nTriangle")
     tri.area()
+elif(ch == 2):
+    print("\nSquare")
+    sqr.area()
+elif(ch == 3):
+    print("\nPentagon")
+    pent.area()
+elif(ch == 4):
+    print("\nHexagon")
+    hex.area()
 else:
     print("I said 1, 2, 3, or 4...")
