0383ef757ad07c9d0514e30a6218ee82c7db3cfa
Siebel Suhail <72099341+TheBestCoderInTheWorld123@users.noreply.github.com>
2025-01-15 22:53:34 +0500

    Create LCM.py

---
+++ ACPS/LCM.py
@@ -0,0 +1,23 @@
+num1 = int(input("Enter first number\n"))
+num2 = int(input("Enter second number\n"))
+
+if(num1 > num2):
+    big_num = num1
+    smoll_num = num2
+else:
+    big_num = num2
+    smoll_num = num1
+
+og_num1 = num1
+og_num2 = num2
+
+while(smoll_num):
+    temp = smoll_num
+    smoll_num = big_num % smoll_num
+    big_num = temp
+
+hcf = big_num
+
+lcm = (og_num1 * og_num2) // hcf
+
+print(f"LCM of {num1} and {num2} is", lcm)
