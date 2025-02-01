0432dea4d56289265070b67475354dbf8290befa
Siebel Suhail <72099341+TheBestCoderInTheWorld123@users.noreply.github.com>
2025-02-01 13:00:40 +0500

    Create BitIsEqualTo.py

---
+++ Projects/BitIsEqualTo.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter number 1\n"))
+num2 = int(input("Enter number 2\n"))
+
+if(num1 ^ num2 != 0):
+    print(f"{num1} and {num2} aren't equal")
+else:
+    print(f"{num1} and {num2} are equal")
